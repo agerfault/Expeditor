5a5a973b4b3fc7a4b0c6fe28ab8f5c0340cea898
Kevin Desmay <kevin.desmay@gmail.com>
2016-06-20 16:09:49 +0200

    Creation du bundle

---
+++ src/ExpeditorBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ExpeditorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ExpeditorBundle:Default:index.html.twig');
+    }
+}
